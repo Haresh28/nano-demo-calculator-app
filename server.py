e298618381b4335d8f67c1d3b216da6defa51688
Haresh Sundararajan <sundarharesh2810@gmail.com>
2023-08-07 16:59:25 +0530

    new commit

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'Hello World'
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
